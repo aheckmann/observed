df94979c33ac67bb6f3dbf978bb3567f1d20710c
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2013-10-21 22:06:11 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,139 @@
+#observed
+==========
+
+ES6 `Object.observe` with nested object support; e.g. the way I want it.
+
+## what?
+
+Do you dream of observing a plain javascript object for changes and reacting to it later? Now you can.
+
+Hiding being the `--harmony` flag of Node 0.11, the [Object.observe](http://wiki.ecmascript.org/doku.php?id=harmony:observe) gem resides.
+
+`Object.observe` allows us to register a listener for any type of change to a given object.
+
+```js
+// node --harmony
+
+var o = { name: 'harmony' };
+Object.observe(o, function (changes) {
+  console.log(changes);
+})
+o.name = 'ES6!'
+o.kind = 'observed';
+
+// logs..
+// [ { type: 'updated',
+//     object: { name: 'ES6!', kind: 'observed' },
+//     name: 'name',
+//     oldValue: 'harmony' },
+//   { type: 'new',
+//     object: { name: 'ES6!', kind: 'observed' },
+//     name: 'kind' } ]
+```
+
+You'll notice our callback received an array of all changes that occured. Cool.
+But what about nested objects? Do they get automatically observed as well?
+
+```js
+// node --harmony
+
+var o = { nested: { deeper: true }};
+Object.observe(o, function (changes) {
+  console.log(changes);
+})
+o.nested.deeper = false
+// crickets ..
+```
+
+Turns out they don't. That's what `observed` is for: watching objects for changes without having to care about whether or not they have nested objects and arrays.
+
+## usage
+
+`observed` returns an `EventEmitter` which you listen to for changes.
+There are four classes of events, mirroring `Object.observe`
+
+- `new`
+- `updated`
+- `deleted`
+- `reconfigured`
+
+```js
+var O = require('observed')
+var object = { name: {} }
+var ee = O(object)
+
+ee.on('new', console.log)
+
+object.name.last = 'observed'
+
+// logs
+// { path: 'name.last',
+//   name: 'last',
+//   type: 'new',
+//   object: { last: 'observed' },
+//   value: 'observed',
+//   oldValue: undefined }
+```
+
+You'll notice we now receive more information compared to `Object.observe`
+
+- `path`: full path to the property, including nesting
+- `name`: name of the path reported by `Object.observe`
+- `type`: name of the event reported by `Object.observe`
+- `object`: object reported from `Object.observe`
+- `value`: current value for the given `path`. same as `object[name]`
+- `oldValue`: previous value of the property as reported by `Object.observe`
+
+You may also listen for changes to specific paths:
+
+```js
+var O = require('observed')
+var object = { name: { last: 'Heckmann', first: 'aaron' }}
+var ee = O(object)
+
+ee.on('updated name.first', console.log)
+
+object.name.first = 'Aaron'
+
+// logs
+// { path: 'name.first',
+//   name: 'first',
+//   type: 'updated',
+//   object: { last: 'Heckmann', first: 'Aaron' },
+//   value: 'Aaron',
+//   oldValue: 'aaron' }
+```
+
+## use cases
+
+1. passing object changes down to a browser in realtime using something like [primus](https://github.com/primus/primus).
+2. fanning out object changes across multiple nodes using something like [axon](https://github.com/visionmedia/axon).
+3. buffering changes and pass them off to your database of choice in one `save` action.
+
+## features
+
+1. Object tracking: Using ES6 `Object.observe` we provide support for rich object tracking without manual getters/setters.
+2. Unobtrusive: Your object remains untouched and you may work with it as a plain js object.
+3. Events: Receive an `EventEmitter` back which emits the following events:
+
+- `added`
+- `removed`
+- `changed`
+- `reconfigured`
+
+## harmonious
+
+You must run node using the `--harmony` flag to use this module.
+
+```
+> node --harmony yourProgram.js
+```
+
+## tests
+
+Run em with `npm test`
+
+## license
+
+[MIT](https://github.com/aheckmann/observed/blob/master/LICENSE)
+
---
+++ test/index.js
@@ -0,0 +1,212 @@
+
+var O = require('../');
+var assert = require('assert');
+
+describe('observed', function(){
+  it('is a function', function(done){
+    assert.equal('function', typeof O);
+    done();
+  })
+
+  it('returns an event emitter', function(done){
+    var ee = O({});
+    assert.equal('function', typeof ee.on);
+    assert.equal('function', typeof ee.emit);
+    assert.equal('function', typeof ee.addListener);
+    done();
+  })
+
+  it('emits "new" events', function(done){
+    var o = {};
+    var ee = O(o);
+
+    ee.on('new', function (change) {
+      assert.equal('name', change.path);
+      assert.equal('observed', change.value);
+      done();
+    });
+
+    o.name = 'observed';
+  })
+
+  it('emits "updated" events', function(done){
+    var o = {};
+    var ee = O(o);
+
+    ee.on('updated', function (change) {
+      assert.equal('name', change.path);
+      assert.equal('second', change.value);
+      assert.equal(change.value, change.object.name);
+      done();
+    });
+
+    o.name = 'first';
+    o.name = 'second';
+  })
+
+  it('emits "updated field" events', function(done){
+    var o = {};
+    var ee = O(o);
+
+    ee.on('updated name', function (change) {
+      assert.equal('second', change.value);
+      assert.equal(change.value, change.object.name);
+      done();
+    });
+
+    o.name = 'first';
+    o.name = 'second';
+  })
+
+  it('emits "reconfigured" events', function(done){
+    var o = {};
+    var ee = O(o);
+
+    ee.on('reconfigured', function (change) {
+      assert.equal('name', change.path);
+      assert.equal('second', change.value);
+      assert.equal(change.value, change.object.name);
+      done();
+    });
+
+    o.name = 'first';
+    Object.defineProperty(o, 'name', { get: function(){ return 'second' }});
+  })
+
+  it('emits "deleted" events', function(done){
+    var o = {};
+    var ee = O(o);
+
+    ee.on('deleted', function (change) {
+      assert.equal('name', change.path);
+      assert.equal(undefined, change.value);
+      assert.equal(change.value, change.object.name);
+      done();
+    });
+
+    o.name = 'first';
+    delete o.name;
+  })
+
+  describe('supports deeply nested objects', function(){
+    var o = {
+        nested: {
+            tags: ['logic audio', 'native instruments', 'drums']
+          , deeper: [{ x: 3 }, { deeperer: [47,108] }]
+        }
+      , array: [new Date, new Buffer(0)]
+    };
+
+    var ee = O(o);
+
+    it('one level deep', function(done){
+      ee.once('new', function (o) {
+        assert.equal('nested.name', o.path);
+        assert.equal('observed', o.value);
+        assert.equal(o.value, o.object.name);
+
+        o.object.name = 'changed';
+      });
+
+      ee.once('updated nested.name', function (o) {
+        assert.equal('changed', o.value);
+        assert.equal(o.value, o.object.name);
+        done();
+      });
+
+      o.nested.name = 'observed';
+    })
+
+    it('two levels deep', function(done){
+      ee.once('new', function (change) {
+        assert.ok(Array.isArray(change.object));
+        assert.equal('nested.tags.3', change.path);
+        assert.equal('cajon', change.value);
+        assert.equal(undefined, change.oldValue);
+      });
+
+      ee.once('updated nested.tags.length', function (change) {
+        assert.equal('nested.tags.length', change.path);
+        assert.equal('length', change.name);
+        assert.equal(4, change.value);
+        assert.equal(3, change.oldValue);
+        done();
+      });
+
+      o.nested.tags.push('cajon');
+    })
+
+    it('three levels deep', function(done){
+      ee.once('new', function (change) {
+        assert.equal('name', change.name);
+        assert.equal('nested.deeper.0.name', change.path);
+        assert.equal('array of objects', change.value);
+        assert.equal(undefined, change.oldValue);
+        done();
+      });
+
+      o.nested.deeper[0].name = 'array of objects';
+    })
+
+    /* TODO
+    it('emits generic array path changes', function(done){
+      ee.once('updated nested.deeper.x', function(change){
+        assert.equal(1, change.index);
+        done();
+      })
+      o.nested.deeper[1].x = 5;
+    })
+    */
+
+    it('begins listening to newly added objects', function(done){
+      o.newlyAddedObject = { x: [[{ woot: true }]] };
+
+      ee.once('new', function(change){
+        o.newlyAddedObject.x[0][0].woot = false;
+      });
+
+      ee.once('updated newlyAddedObject.x.0.0.woot', function(change){
+        assert.strictEqual(false, change.value);
+        assert.strictEqual(true, change.oldValue);
+        done();
+      });
+    })
+  })
+
+  it('stops listening to deleted properties', function(done){
+    var o = { nil: null, name: { last: 'h', first: 'a' }};
+    var ee = O(o)
+    assert.equal(2, ee.observers.length);
+    ee.once('deleted', function(change){
+      assert.equal(1, ee.observers.length);
+
+      ee.once('deleted', function(change){
+        done();
+      });
+
+      delete o.nil;
+    });
+    delete o.name;
+  })
+
+  it('listens to array manipulation', function(done){
+    var o = [];
+    var ee = O(o);
+    ee.once('new', function (change) {
+      assert.equal(3, change.value);
+
+      var times = 0;
+
+      // update is called once per added element
+      var onupdate = function (change) {
+        if (++times == 4) {
+          assert.deepEqual([3,4,5,6,7], o);
+          done();
+        }
+      }
+      ee.on('new', onupdate);
+      o.push(4,5,6,7);
+    })
+    o.push(3);
+  })
+})
